5084e420f544f8a3cd4ac7c9f3aa1b13aa2b4392
haneda <18521488+ap8322@users.noreply.github.com>
2018-10-13 19:29:10 +0900

    add cors setting in backend

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"github.com/iris-contrib/middleware/cors"
 	"github.com/kataras/iris"
 )
 
 func main() {
 	app := iris.Default()
+
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"http://localhost:8000"},
+	})
+	app.Use(c)
+
 	app.Get("/health", func(ctx iris.Context) {
 		ctx.JSON(iris.Map{
 			"message": "ok",
